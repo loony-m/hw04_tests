77b2fab3ed5b805761dc6f2ea5314ede79a3ba73
loony <wooounding@gmail.com>
2022-11-23 14:51:08 +0300

    добавил тесты для форм постов

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from django.urls import reverse
@@ -14,15 +15,19 @@ class FormTest(TestCase):
         super().setUpClass()
 
         cls.user = User.objects.create(username='test')
+        cls.user_no_have_post = User.objects.create(username='test_2')
+        cls.guest_client = Client()
         cls.authorized_client = Client()
         cls.authorized_client.force_login(cls.user)
+        cls.authorized_client_nohave_post = Client()
+        cls.authorized_client_nohave_post.force_login(cls.user_no_have_post)
 
         cls.post = Post.objects.create(
             text='Новый пост',
             author=cls.user
         )
 
-    def test_add_post(self):
+    def test_add_post_authorized_client(self):
         post_count = Post.objects.count()
 
         form_field = {
@@ -38,7 +43,25 @@ class FormTest(TestCase):
 
         self.assertEqual(Post.objects.count(), post_count + 1)
 
-    def test_edit_post(self):
+    def test_add_post_guest_client(self):
+        form_field = {
+            'text': 'Новый пост 3',
+            'author': FormTest.guest_client
+        }
+
+        before_post_count = Post.objects.count()
+        
+        FormTest.guest_client.post(
+            reverse('posts:post_create'),
+            data=form_field,
+            follow=True
+        )
+
+        after_post_count = Post.objects.count()
+
+        self.assertEqual(before_post_count, after_post_count)
+
+    def test_edit_post_authorized_client(self):
         form_edited_field = {
             'text': 'Новый пост 3',
             'author': FormTest.authorized_client
@@ -53,3 +76,13 @@ class FormTest(TestCase):
         post_edited = Post.objects.get(id=FormTest.post.id)
 
         self.assertEqual(post_edited.text, form_edited_field['text'])
+
+    def test_edit_post_guest_client(self):
+        response = FormTest.guest_client.get('/posts/1/edit/')
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND.value)
+
+    def test_edit_no_have_post_authorized_client(self):
+        response = FormTest.authorized_client_nohave_post.get('/posts/1/edit/')
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND.value)
